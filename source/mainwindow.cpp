bdea607d6be6a24338987a714b832fc57ac5ead9
Wang Xin <2364037658@qq.com>
2022-11-23 16:56:56 +0800

    Change to English

--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -15,15 +15,15 @@ MainWindow::MainWindow(QWidget *parent)
       menuBar(); //返回主窗口的菜单栏。如果菜单栏不存在，此函数将创建并返回一个空菜单栏。
 
   // 菜单栏上添加一个 "文件" 的菜单
-  QMenu *pFile = mBar->addMenu("文件");
+  QMenu *pFile = mBar->addMenu("Files");
 
   //添加 "打开" 菜单项
-  QAction *pOpen = pFile->addAction("打开");
+  QAction *pOpen = pFile->addAction("Open");
   connect(pOpen, &QAction::triggered, [=]() { qDebug() << "打开按下"; });
 
   pFile->addSeparator(); //添加分割线
 
-  QAction *pSave = pFile->addAction("保存"); //添加 "保存" 菜单项动作
+  QAction *pSave = pFile->addAction("Save"); //添加 "保存" 菜单项动作
 
   // 新建菜单选项的触发动作信号和槽 使用Lambda表达式的匿名槽函数
   connect(pSave, &QAction::triggered, // "保存" 按下后的动作打印信息
@@ -31,7 +31,7 @@ MainWindow::MainWindow(QWidget *parent)
 
   pFile->addSeparator(); //添加分割线
 
-  QAction *pNewSave = pFile->addAction("另存为"); //添加 "另存为" 菜单项动作
+  QAction *pNewSave = pFile->addAction("Save as"); //添加 "另存为" 菜单项动作
 
   // 新建菜单选项的触发动作信号和槽 使用Lambda表达式的匿名槽函数
   connect(pNewSave, &QAction::triggered, // "另存为" 按下后的动作打印信息
