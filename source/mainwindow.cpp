1399a71b67d12658d20df9d51b07be6b43658b4a
Yusen Zheng <ysyszheng@gmail.com>
2022-11-23 15:49:59 +0800

    update

--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -1,60 +1,41 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <QMenuBar>
-#include <QMenu>
 #include <QAction>
 #include <QDebug>
+#include <QMenu>
+#include <QMenuBar>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
-{
-    ui->setupUi(this);
-    setWindowTitle(tr("Sniffer X"));
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
+  ui->setupUi(this);
+  setWindowTitle(tr("Sniffer X"));
 
-    // 添加菜单栏
-    QMenuBar *mBar = menuBar();//返回主窗口的菜单栏。如果菜单栏不存在，此函数将创建并返回一个空菜单栏。
+  // 添加菜单栏
+  QMenuBar *mBar =
+      menuBar(); //返回主窗口的菜单栏。如果菜单栏不存在，此函数将创建并返回一个空菜单栏。
 
-    // 菜单栏上添加一个 "文件" 的菜单
-    QMenu *pFile = mBar->addMenu("文件");
+  // 菜单栏上添加一个 "文件" 的菜单
+  QMenu *pFile = mBar->addMenu("文件");
 
-    //添加 "打开" 菜单项
-    QAction *pOpen = pFile->addAction("打开");
-    connect(pOpen,&QAction::triggered,
-            [=] ()
-            {
-                qDebug()<<"打开按下";
-            }
-            );
+  //添加 "打开" 菜单项
+  QAction *pOpen = pFile->addAction("打开");
+  connect(pOpen, &QAction::triggered, [=]() { qDebug() << "打开按下"; });
 
-    pFile->addSeparator();//添加分割线
+  pFile->addSeparator(); //添加分割线
 
-    QAction *pSave = pFile->addAction("保存");//添加 "保存" 菜单项动作
+  QAction *pSave = pFile->addAction("保存"); //添加 "保存" 菜单项动作
 
-    // 新建菜单选项的触发动作信号和槽 使用Lambda表达式的匿名槽函数
-    connect(pSave,&QAction::triggered,// "保存" 按下后的动作打印信息
-            [=] ()
-            {
-                qDebug()<<"保存按下";
-            }
-            );
+  // 新建菜单选项的触发动作信号和槽 使用Lambda表达式的匿名槽函数
+  connect(pSave, &QAction::triggered, // "保存" 按下后的动作打印信息
+          [=]() { qDebug() << "保存按下"; });
 
-    pFile->addSeparator();//添加分割线
+  pFile->addSeparator(); //添加分割线
 
+  QAction *pNewSave = pFile->addAction("另存为"); //添加 "另存为" 菜单项动作
 
-    QAction *pNewSave = pFile->addAction("另存为");//添加 "另存为" 菜单项动作
-
-    // 新建菜单选项的触发动作信号和槽 使用Lambda表达式的匿名槽函数
-    connect(pNewSave,&QAction::triggered,// "另存为" 按下后的动作打印信息
-            [=] ()
-            {
-                qDebug()<<"另存为按下";
-            }
-            );
-
+  // 新建菜单选项的触发动作信号和槽 使用Lambda表达式的匿名槽函数
+  connect(pNewSave, &QAction::triggered, // "另存为" 按下后的动作打印信息
+          [=]() { qDebug() << "另存为按下"; });
 }
 
-MainWindow::~MainWindow()
-{
-    delete ui;
-}
+MainWindow::~MainWindow() { delete ui; }
