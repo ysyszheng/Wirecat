47c828c81474f2ec928eb9349069d9bf095710da
Yusen Zheng <ysyszheng@gmail.com>
2022-11-24 17:03:09 +0800

    modify test file

--- test/test.cpp
+++ test/test.cpp
@@ -1,10 +1,14 @@
 #include "../src/sniffer.h"
 #include "../src/utils.h"
-#include <iostream>
-#include <pcap.h>
 
 int main(int argc, char **argv) {
   Sniffer sniffer;
   sniffer.findAllDevs();
+
+  const char *devName = "eth0";
+  sniffer.getDevName(devName);
+  sniffer.getDevInfo();
+  sniffer.sniff();
+
   return 0;
 }
