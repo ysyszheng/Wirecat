94838a909d478f764ce79e3b6ab78818b8a600fc
Yusen Zheng <ysyszheng@gmail.com>
2022-11-25 12:54:09 +0800

    modify test file

--- test/test.cpp
+++ test/test.cpp
@@ -2,16 +2,16 @@
 #include "../src/utils/utils.h"
 
 int main(int argc, char **argv) {
-  // Sniffer sniffer;
-  // sniffer.findAllDevs();
+  Sniffer sniffer;
+  sniffer.findAllDevs();
 
-  // const char *devName = "eth0";
-  // sniffer.getDevName(devName);
-  // sniffer.getDevInfo();
-  // sniffer.sniff();
+  const char *devName = "eth0";
+  sniffer.getDevName(devName);
+  sniffer.getDevInfo();
+  sniffer.sniff();
 
-  const u_char c[] = "www.baidu.com\0\0\0\0\0[[[[[]]]]]\0s\0[>\0ab45";
-  print_payload(c, 38);
+  // const u_char c[] = "\0\0<]\0D\0\0**#\0\0\0E\0\0)\0\0@";
+  // print_payload(c, 21);
 
   return 0;
 }
